0fad74c08d4376b0bbb30d737e207c6651578b6b
samyravitoria <49448093+samyravitoria@users.noreply.github.com>
2019-06-29 08:25:02 -0300

    Update chase.cpp

--- CEOI/2017/chase.cpp
+++ CEOI/2017/chase.cpp
@@ -1,15 +1,53 @@
 /*
-  CEOI 2017 - Task Chase
-  Solving by Samyra Almeida
+	CEOI 2017 - Task Chase
+	Solving by Samyra Almeida
   
-  First, I'll do the array vis[], where vis[u] = sum of the number of pigeons in the statues around u. And with a dfs I'll do the 
-  dp[u][i] = the biggest difference between the children of u, the number of pigeons that are seen by Tom and Jerry from the root 
-  (first statue of the way) to the u statue, still having i crumbs and dp2[u][i] = the second biggest difference, from a child other 
-  than u, to the number of pigeons that are seen by Tom and Jerry from the root (first statue of the path) to the x statue, still 
-  having i crumbs.
-  Now, I'll a recursive funcion, very similar wiht dfs, that have 2 parameters rotate(u, f) where u is the actual tree root and f is 
-  the u "father". First I'll check dp[u][i] is the best answer for all i in the range 1 ... k. 
-    
+	Some notes: 
+    	- vet[u] = number of pigeons in the u statue.
+    	
+		- vis[u] = sum of the number of pigeons in the statues around u.
+    	
+		- Each statue is a node of a tree and a way between two statues is a edge.
+  	
+	I'll do a dynamic programming, in the follow way:
+    	- dp[u][i] = maximum difference of the number of pigeons that are seen by Tom and Jerry from the root (first statue of the way) 
+		to the u statue, still having i crumbs, between the childrem of u.
+    	
+		- dp2[u][i] = second maximum difference (of a different node than dp[u][i]) of the number of pigeons that are seen by Tom and 
+		Jerry from the root (first statue of the way) to the u statue, still having i crumbs, between the childrem of u.
+    	
+		- the dp's have 2 cases (for all i in range 1 to k), to a node v (child of u): 
+			
+			- if dp[u][i] < max(dp[v][i], dp[v][i] + vis[v] - vet[u]):
+				update the dp2[u][i] = dp[u][i] and dp[u][i] = max(dp[v][i], dp[v][i] + vis[v] - vet[u]).
+			
+			- else if dp2[u][i] < max(dp[v][i], dp[v][i] + vis[v] - vet[u]):
+				update the dp2[u][i] = max(dp[v][i], dp[v][i] + vis[v] - vet[u]).
+	
+		*To do the dp's we use a DFS in the tree, but for check the answer is necessary do all nodes as root with runs in O(n*n) and 
+		this solution will receive TLE.
+		
+		So, rotate the tree in a smart way is the solution. But, how? Think a bit about it!
+			- For all node u, do the your children v as root (for all i in range 1 to k): 
+				- If the dp[u][i] == max(dp[v][i], dp[v][i-1] + vis[v] - vet[u]) the dp[u][i] depend of v:
+					So, updade the dp[u][i] to dp2[u][i].
+					
+				- After, update de dp and dp2 of v, because now u is a children of v (for all i in range 1 to k): 
+					- if dp[v][i] < max(dp[u][i], dp[u][i] + vis[u] - vet[v]):
+						update the dp2[u][i] = dp[u][i] and dp[u][i] = max(dp[u][i], dp[u][i] + vis[u] - vet[v]).
+			
+					- else if dp2[u][i] < max(dp[u][i], dp[u][i] + vis[u] - vet[v]):
+						update the dp2[u][i] = max(dp[u][i], dp[u][i] + vis[u] - vet[v]).
+		  	
+			*To runs the above algorithm do a function much similar to DFS in a tree, and for give the answer, in the top of the 
+			function before go through the children of u, do (for all i in range 1 to k): 
+		
+				- Do it: ans = max({ans, dp[u][i], dp[u][i-1] + vis[u]}), in the other words, the maximum between the previus answer, the 
+				maximum difference of the number of pigeons that are seen by Tom and Jerry from the u to the some node v in the subtree 
+				of u still having i crumbs and the maximum difference of the number of pigeons that are seen by Tom and Jerry from the u 
+				to the some node v in the subtree of u still having i-1 crumbs (dropped a crumb in the u).
+		
+		To finish, just print the ans.
 */
  
 #include <bits/stdc++.h>
